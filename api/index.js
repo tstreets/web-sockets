7845af597124013dd2012a101c7a88f47a46123a
Ty Streets <tstreets@iu.edu>
2024-10-23 16:19:04 -0400

    Made api routes for getting and adding real data

--- api/index.js
+++ api/index.js
@@ -2,4 +2,17 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const buttonData = ["red", "green", "red"];
+
+app.get("/api/buttons/data", function (req, res) {
+  res.status(200).json(buttonData);
+});
+
+app.post("/api/buttons/data/new", function (req, res) {
+  buttonData.push(req.body.value);
+  res.status(200).json(buttonData);
+});
+
 module.exports = app;
--- views/app.js
+++ views/app.js
@@ -7,3 +7,26 @@ const chart = new Chart(graphRef, {
     datasets: [{ label: "Buttons", data: [2, 7] }],
   },
 });
+
+async function getData() {
+  const buttonReq = await fetch(`/api/buttons/data`);
+  const buttonData = await buttonReq.json();
+
+  const colors = {};
+  // { "red": 2, "green": 1}
+
+  for (let i = 0; i < buttonData.length; i++) {
+    const buttonColor = buttonData[i];
+    // colors[buttonColor] = colors[buttonColor] || 0;
+    colors[buttonColor] ||= 0;
+    colors[buttonColor]++;
+  }
+
+  chart.data = {
+    labels: Object.keys(colors),
+    datasets: [{ label: "Buttons", data: Object.values(colors) }],
+  };
+  chart.update();
+}
+
+getData();
